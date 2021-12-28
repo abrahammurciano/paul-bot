b8a1c3841394e10726278c6c2595ae174f0bc0a5
Abraham Murciano <abrahammurciano@gmail.com>
2021-12-28 19:36:17 +0200

    added channel log handler

--- main.py
+++ main.py
@@ -2,31 +2,33 @@ import asyncio
 import os
 import application
 from presentation.paul import Paul
+from presentation.discord_channel_handler import DiscordChannelHandler
 import logging
 import tracemalloc
 
+# empty space effectively disables prefix since discord strips trailing spaces
+bot = Paul(" ")
+
 logging.basicConfig(
-	format="%(asctime)s %(levelname)-8s %(message)s",
-	level=logging.INFO,
-	datefmt="%Y-%m-%d %H:%M:%S",
-	handlers=[
-		logging.FileHandler("ouput.log"),
-		logging.StreamHandler()
-	]
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("ouput.log"),
+        logging.StreamHandler(),
+        DiscordChannelHandler(bot, int(os.getenv("LOG_CHANNEL", ""))),
+    ],
 )
 tracemalloc.start()
 
 
 async def main():
-	# Connect to the database
-	await application.init()
+    # Connect to the database
+    await application.init()
 
-	# empty space effectively disables prefix since discord strips trailing spaces
-	bot = Paul(" ")
-
-	# Run Discord bot
-	await bot.start(os.getenv("BOT_TOKEN", ""))
+    # Run Discord bot
+    await bot.start(os.getenv("BOT_TOKEN", ""))
 
 
 if __name__ == "__main__":
-	asyncio.run(main())
+    asyncio.run(main())
